364957d13b0cf1f6aafb6e5ba0d6721e7ca7c93a
amruthexe <aluriamruthrajit@gmail.com>
2026-01-16 00:06:31 +0530

    Convert 3_variables from gitlink to normal folder

---
+++ 3_variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name = "amruth"
+	// fmt.Println(name)
+
+
+	// var name = "amruth"
+	// fmt.Println(name)
+
+	name:="amruth"
+	fmt.Println(name)
+}
