b1c8e4ca9fbe81e413e959f9a3c92704b51b24b8
amruthexe <aluriamruthrajit@gmail.com>
2026-01-16 15:03:01 +0530

    Day 02

---
+++ 6_if_else/if.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// age := 16
+
+	// if age >= 18 {
+	// 	fmt.Println("your adult")
+	// }else {
+	// 	fmt.Println("you small")
+	// }
+
+
+	//or - and 
+
+	// isAdmin:=true
+	// isPremission :=false
+
+	// if isAdmin || isPremission{
+	// 	fmt.Println("you can goo")
+
+	// }
+
+
+
+	if age:=17 ;age >=18{
+		fmt.Println("your adult",age)
+	}else if age >=16{
+		fmt.Println("small teenage person")
+	}
+}
