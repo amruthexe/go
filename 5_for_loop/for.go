7b3f4edaeefabfeef67bf58c4de3511386018b00
amruthexe <aluriamruthrajit@gmail.com>
2026-01-16 15:02:13 +0530

    Day 02

---
+++ 5_for_loop/for.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+
+	// while loop without using while keyword
+
+
+	// i := 0
+	// for i <= 10 {
+	// 	fmt.Println(i)
+	// 	i+=1
+	// }
+
+	// for loop ➰
+
+	// for i:=0 ; i<=10 ;i++{
+	// 	// break
+
+	// 	if i==2{
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+
+	for i:= range 10{
+		fmt.Println(i)
+	}
+
+
+
+
+}
